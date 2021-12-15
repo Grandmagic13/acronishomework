57b5d1f5d2bdb30daa31ff09d0fd14aaca11909e
Grandmagic13 <jabencefear@hotmail.com>
2021-12-15 01:07:38 +0100

    add: Implemented new functionalities
    - remote handler now requires login info in constructor
    - implemented connect functionality
    - implemented disconnect functionality
    - implemented remote execution (with option to print out stdin)
    - implemented remote restart

--- linux_remote_handler.py
+++ linux_remote_handler.py
@@ -1,6 +1,31 @@
+import functools
+
+import paramiko as paramiko
+
 from remote_handler_base import RemoteHandlerBase
 
 
 class LinuxRemoteHandler(RemoteHandlerBase):
-    def __init__(self, ip_address):
-        super().__init__(ip_address)
+    def __init__(self, username, password, host, port=22):
+        super().__init__(host)
+        self.port = port
+        self.username = username
+        self.password = password
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    def connect(self):
+        self.client.connect(self.ip_address, self.port, self.username, self.password)
+
+    def disconnect(self):
+        self.client.close()
+
+    def execute_in_commandline(self, command, print_stdout=False):
+        stdin, stdout, stderr = self.client.exec_command(command)
+        if print_stdout:
+            [print(line) for line in stdout.readlines()]
+        return {"stdin": stdin, "stdout": stdout, "stderr": stderr}
+
+    def restart(self):
+        stdin, stdout, stderr = self.client.exec_command("echo {} | sudo -S reboot -f".format(self.password))
+        return {"stdin": stdin, "stdout": stdout, "stderr": stderr}
