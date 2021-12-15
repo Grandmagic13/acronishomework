e72064a759214a5aa764b8ea0799072cc94354b5
Grandmagic13 <jabencefear@hotmail.com>
2021-12-15 06:37:35 +0100

    Add: Implement windefender parameters queries
    - Method to return windefender status params
    - Method to return windefender preferences params
    - Optional argument for printing console output
    - Methods return param key-value pairs in dict
    - Helper functions extracted

--- windows_remote_handler.py
+++ windows_remote_handler.py
@@ -2,6 +2,24 @@ from remote_handler_base import RemoteHandlerBase
 import winrm
 
 
+## Helpers
+
+def print_stdout(command_result):
+    print(command_result.std_out.decode("utf-8"))
+
+
+def extract_key_value_tuple(line):
+    return tuple(map(lambda line: line.strip(), line.split(":", 1)))
+
+
+def generate_dict_out_of(command_result):
+    lines = command_result.std_out.decode("utf-8").split("\r\n")
+    filtered_lines = list(filter(lambda line: len(line) > 0, lines))
+    return dict(
+        map(lambda line: (extract_key_value_tuple(line)), filtered_lines)
+    )
+
+
 class WindowsRemoteHandler(RemoteHandlerBase):
     def __init__(self, username, password, host, domain=None):
         super().__init__(host)
@@ -20,8 +38,21 @@ class WindowsRemoteHandler(RemoteHandlerBase):
     def power_off(self):
         return self.execute_in_commandline("shutdown /p")
 
-    def get_windefender_params(self):
-        raise NotImplementedError()
+    ## wasn't sure which windows defender parameters the task was referring to so I'm implementing both
+
+    # returns dict of antimalware status key-value pairs and optionally prints command's console output
+    def get_windefender_antimalware_status(self, print_readable=False):
+        result = self.execute_in_commandline("Get-MpComputerStatus")
+        if print_readable:
+            print_stdout(result)
+        return generate_dict_out_of(result)
+
+    # returns dict of windows defender preferences key-value pairs and optionally prints command's console output
+    def get_windefender_params(self, print_readable=False):
+        result = self.execute_in_commandline("Get-MpPreference")
+        if print_readable:
+            print_stdout(result)
+        return generate_dict_out_of(result)
 
     # not sure about what this one has to do exactly yet, assuming it has to return a list of registry keys
     def get_registry_keys(self):
